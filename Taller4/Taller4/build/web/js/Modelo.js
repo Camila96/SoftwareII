e12126fa783e9004c9babb74646fd1a2abfe9af4
Camila96 <maria.preciado01@hotmail.com>
2017-09-20 13:25:32 -0500

    Add files via upload

---
+++ Taller4/Taller4/build/web/js/Modelo.js
@@ -0,0 +1,31 @@
+var idFacultad=3;
+var facultades=[
+    {"id":1, "nombre":"Ingenieria"},
+    {"id":2, "nombre":"Licenciatura"}
+
+];
+var idCarrera=1;
+var carreras=[
+    
+];
+var idEstudiante=3;
+var estudiantes=[
+    
+];
+
+var idProfesor=6;
+var profesores=[
+    
+];
+
+var idMateria=7;
+var materias=[
+    
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
